1ad74caa1f60956e488edbd2e3cf96ca3b0ba248
Shanise Barona <shanisebarona@gmail.com>
2020-01-07 02:48:01 +0000

    Automatically backed up by Learn

---
+++ new.rb
@@ -0,0 +1,15 @@
+class Pet
+  attr_reader :breed
+  
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  # def breed
+  #   @breed
+  # end
+  
+  # def breed=(breed)
+  #   @breed = breed
+  # end
+end
